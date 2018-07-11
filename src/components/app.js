4ddb7823794b748b858638f5e44537a6fe840918
Blake Doty <blaked609@gmail.com>
2018-07-11 11:45:03 -0600

    Added missing code from videos, refactored app.js and picker.js to have startdate

--- src/components/app.js
+++ src/components/app.js
@@ -1,31 +1,113 @@
 import React, { Component } from 'react';
 import Picker from './picker';
-import  Button  from './button';
+import Button from './button';
 import Clock from './clock';
 import changeDate from './changeDate';
 import LargeText from './largeText';
+import moment from 'moment';
 
 export default class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      active: false
-    }
+      active: false,
+      startDate: moment(),
+      timeRemaining: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      },
+      age: 0
+    };
+
+    this.handleGenerate = this.handleGenerate.bind(this);
   }
 
-  renderItems = function() {
-    if(this.state.active) {
+  handleChange = function(date) {
+    console.log('APP JS HANDLE CHANGE', date._d);
+    this.setState({
+      startDate: date
+    });
+  }.bind(this);
+
+  handleGenerate = function() {
+    clearInterval(this.timer)
+
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
+
+    var timeBetween = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(timeBetween / (100 * 60 * 60 * 24));
+    var age = Number((daysOld/365).toFixed(0));
+    this.setState({
+      age,
+      active: true
+    });
+
+    if(birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      }
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      }
+    }
+
+    var countDownDate = bday.getTime();
+
+    this.timer = setInterval(function() {
+
+      var now = moment().toDate().getTime();
+      var distance = countDownDate - now;
+
+      var days = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+      const timeRemaining = {
+        days,
+        hours,
+        minutes,
+        seconds
+      };
+
+      this.setState({ timeRemaining });
+
+      if(distance < 0) {
+        clearInterval(this.timer);
+      }
+
+
+    }.bind(this), 1000);
+  }.bind(this);
+
+  renderItems = function () {
+    if (this.state.active) {
       return [
         <Clock />,
-        changeDate('Change Date', () => this.setState({active: false})),
+        changeDate('Change Date', () => this.setState({ active: false })),
         LargeText('04/03'),
         <label className="grid__remaining">Remaining until your 21st birthday</label>
       ]
     } else {
       return [
-        <Picker />,
-        Button("Generate Countdown", () => this.setState({active: true}))
+        <Picker callback={(date) => this.handleChange(date)}/>,
+        Button("Generate Countdown", () => this.setState({ active: true }))
       ]
     }
   }.bind(this)
@@ -41,8 +123,8 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
 
-        { this.renderItems() }
-        
+        {this.renderItems()}
+
       </div>
     );
   }
--- src/components/picker.js
+++ src/components/picker.js
@@ -19,6 +19,7 @@ export default class Picker extends Component {
         this.setState({
             startDate: date
         });
+        this.props.callback(date)
     }
 
 
